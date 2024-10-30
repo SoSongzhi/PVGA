511da75821082033dc38e89348489283539254d5
ZhiSong <74604653+SoSongzhi@users.noreply.github.com>
2024-10-30 21:00:02 +0800

    Update writesequence.py

--- writesequence.py
+++ writesequence.py
@@ -1,20 +1,20 @@
 import os
 
 def store_labels_as_fa(labels, folder):
-    # 创建文件夹
+    
 
     os.makedirs(folder, exist_ok=True)
     for i, sequence in enumerate(labels):
-        trimmed_sequence = sequence  # Remove first and last letters
+        trimmed_sequence = sequence 
         if sequence[0] == 'E' or sequence[0] == 'B':
             trimmed_sequence = sequence[1:]
 
         if sequence[-1] == 'E' or sequence[-1] == 'B':
             trimmed_sequence = trimmed_sequence[:-1]
 
-        trimmed_sequence_str = ''.join(trimmed_sequence)  # Convert list to string
-        reversed_sequence = trimmed_sequence_str[::-1]  # Reverse the sequence
-        # 写入文件
+        trimmed_sequence_str = ''.join(trimmed_sequence)  
+        reversed_sequence = trimmed_sequence_str[::-1]  
+        
         graph_prefix = os.path.basename(os.path.dirname(folder))
         file_path = os.path.join(folder, 'output_{}_{}.fa'.format(graph_prefix, i))
         with open(file_path, 'w') as fa_file:
@@ -26,25 +26,24 @@ def store_labels_as_fa(labels, folder):
 
 
 def store_labels_as_fa_hanshuming(labels, folder, filename_prefix):
-    # 创建文件夹
+   
     os.makedirs(folder, exist_ok=True)
 
     for i, sequence in enumerate(labels):
-        trimmed_sequence = sequence  # 初始为完整序列
+        trimmed_sequence = sequence  
 
-        # 移除开头和结尾的 'E' 或 'B'
+       
         if sequence[0] == 'E' or sequence[0] == 'B':
             trimmed_sequence = sequence[1:]
         if sequence[-1] == 'E' or sequence[-1] == 'B':
             trimmed_sequence = trimmed_sequence[:-1]
 
-        trimmed_sequence_str = ''.join(trimmed_sequence)  # 转换为字符串
-        reversed_sequence = trimmed_sequence_str[::-1]  # 反转序列
+        trimmed_sequence_str = ''.join(trimmed_sequence) 
+        reversed_sequence = trimmed_sequence_str[::-1]  
 
-        # 构建文件路径，使用传入的 filename_prefix 参数
+        
         file_path = os.path.join(folder, f'{filename_prefix}_{i}.fa')
 
-        # 写入文件
         with open(file_path, 'w') as fa_file:
             fa_file.write(f'>{filename_prefix}_sequence{i}    num:{len(trimmed_sequence)}\n')
             fa_file.write(reversed_sequence + '\n')
